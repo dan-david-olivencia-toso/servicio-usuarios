51aa5c5c1027b07cd711c1a5aa3c731ad149aec5
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2021-03-24 19:21:03 -0300

    Commit inicial

---
+++ src/test/java/com/dan/dot/lab01/Lab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.dot.lab01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
